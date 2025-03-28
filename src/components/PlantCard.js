e98b4435824c8a9bb7fb6973717a029265ed7f3b
Danish <dkhans2001@gmail.com>
2025-03-28 03:18:03 -0500

    plants

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ name, image, price }) {
+  const [inStock, setInStock] = useState(true);
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+      <button
+        className={inStock ? "primary" : ""}
+        onClick={() => setInStock(!inStock)}
+      >
+        {inStock ? "In Stock" : "Out of Stock"}
+      </button>
     </li>
   );
 }
